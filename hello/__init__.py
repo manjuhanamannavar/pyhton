4dbb2cdda13b43b221e8a7cea003f22f70eccd8f
manjuhanamannavar <hanaman.manju@gmail.com>
2019-09-12 06:14:17 +0530

    4th commit

--- hello/__init__.py
+++
@@ -1,4 +0,0 @@
-print("Hello Python with GIT")
-a = 10
-print(a)
-b = 20
---
+++ hello/hello.py
@@ -0,0 +1,4 @@
+# This is a comment section and it  is  my first Python program.
+print("Hello Python!\n")
+print ("*" * 10)
+
